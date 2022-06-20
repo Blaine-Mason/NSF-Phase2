ac551a34eff495e161afdfc414c0276fb070b9ff
Blaine Mason <bmason3@gulls.salisbury.edu>
2022-06-20 15:12:56 -0400

    Finished Max Cut

--- .virtual_documents/maxcut.ipynb.py
+++ .virtual_documents/maxcut.ipynb.py
@@ -19,13 +19,40 @@ constraints += [
 ]
 
 objective = sum( .5*(1-X[i, j]) for (i, j) in edges)
-print(f'objective = {objective}')
-prob = cp.Problem(cp.Maximize(objective), constraints)
-print(prob.solve())
 
+prob = cp.Problem(cp.Maximize(objective), constraints)
+prob.solve()
+x = la.sqrtm(X.value)
 evals, evects = np.linalg.eigh(X.value)
 sdp_vectors = evects.T[evals > float(1.0E-6)].T
-print(sdp_vectors)
+vectorss = sdp_vectors
+
+u = np.random.randn(5)
+point  = np.array([0, 0, 0])
+normal = np.array(u @ vectorss)
+signs = np.sign( u @ x)
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+fr = [(0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0)]
+color_dict = ['r' if signs[i] > 0 else 'b' for i in range(len(signs))]
+
+i = 0
+for (from_, sdp_vectors) in zip(fr,sdp_vectors):
+    ax.quiver(*from_, *sdp_vectors, color = color_dict[i])
+    i+= 1
 
 
+# a plane is a*x+b*y+c*z+d=0
+# [a,b,c] is the normal. Thus, we have to calculate
+# d and we're set
+d = -point.dot(normal)
 
+# create x,y
+xx, yy = np.meshgrid(range(-1,2), range(-1,2))
+
+# calculate corresponding z
+z = (-normal[0] * xx - normal[1] * yy - d) * 1. /normal[2]
+
+# plot the surface
+ax.plot_surface(xx, yy, z, alpha=.5, cmap='Greys')
+plt.show()
